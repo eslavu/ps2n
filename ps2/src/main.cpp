a2f2bfb6e3c77566e68ee09bbdceff3da85e745c
Emilia Slavu <slavu.emilia@gmail.com>
2025-05-24 16:50:50 +0300

    structura de baza

---
+++ ps2/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.print("ready");
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
+
